bfe563b6df5129a3339c491c5ed6e4a3149da07f
zhubing <1483664313@qq.com>
2020-04-14 18:04:53 +0800

    consul module

---
+++ cloud-providerconsul-payment8006/src/main/java/com/zhubing/springcloud/PaymentMain8006.java
@@ -0,0 +1,11 @@
+package com.zhubing.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaymentMain8006 {
+    public static void main(String[] args) {
+        SpringApplication.run(PaymentMain8006.class,args);
+    }
+}
